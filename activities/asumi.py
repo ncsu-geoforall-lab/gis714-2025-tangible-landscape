c70886f579099a0b4516aabf7b35a9cda7344190
Asumi Saito <asaito2@ncsu.edu>
2025-02-12 16:17:33 -0500

    added functions

--- activities/asumi.py
+++ activities/asumi.py
@@ -8,6 +8,13 @@ import grass.script as gs
 def run_slope(scanned_elev, env, **kwargs):
     gs.run_command("r.slope.aspect", elevation=scanned_elev, slope="slope", env=env)
 
+def run_watershed(scanned_elev, env, **kwargs):
+    gs.run_command("r.watershed", elevation=scanned_elev, threshold=5000, accumulation="accum_mfd5K", drainage="draindir_5K", basin="basin_mfd5K", env=env)
+
+def run_flow(scanned_elev, env, **kwargs):
+    gs.run_command("r.flow", elevation=scanned_elev, flowline="flowlines", flowlength="flowlg_vb", flowaccumulation="flowacc_vb", env=env)
+
+
 
 def main():
     env = os.environ.copy()
@@ -17,7 +24,8 @@ def main():
     gs.run_command("g.region", raster=elevation, res=4, flags="a", env=env)
     gs.run_command("r.resamp.stats", input=elevation, output=elev_resampled, env=env)
 
-    run_slope(scanned_elev=elev_resampled, env=env)
+    run_watershed(scanned_elev=elev_resampled, env=env)
+    run_flow(scanned_elev=elev_resampled, env=env)
 
 
 if __name__ == "__main__":
