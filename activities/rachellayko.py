591adc2d0c3059229cebab7ba01ef9a600799a5f
rlayko <88908124+rlayko@users.noreply.github.com>
2025-02-26 14:08:01 -0500

    Create points layer, update json
    added points layer code to calculate viewsheds

--- activities/rachellayko.py
+++ activities/rachellayko.py
@@ -4,6 +4,20 @@ import os
 
 import grass.script as gs
 
+def create_end_points(env):
+    info = gs.raster_info("scan")
+    y1 = info["south"] + 2 * (info["north"] - info["south"]) / 10.0
+    y2 = info["south"] + 8 * (info["north"] - info["south"]) / 10.0
+    x1 = info["west"] + 2 * (info["east"] - info["west"]) / 10.0
+    x2 = info["west"] + 8 * (info["east"] - info["west"]) / 10.0
+    gs.write_command(
+        "v.in.ascii",
+        input="-",
+        stdin="{x1}|{y1}\n{x2}|{y2}".format(x1=x1, x2=x2, y1=y1, y2=y2),
+        output="points",
+        env=env,
+    )
+    return ((x1, y1), (x2, y2))
 
 def run_viewshed(scanned_elev, env, **kwargs):
     gs.run_command(
