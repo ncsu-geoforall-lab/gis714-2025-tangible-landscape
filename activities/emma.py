c13a97e8b0d1fe252c86cd8c95a5280b0d8854ef
emmavail <99824891+geoscienceyulei@users.noreply.github.com>
2025-03-07 09:49:52 -0500

    Update emma.py

--- activities/emma.py
+++ activities/emma.py
@@ -1,32 +1,26 @@
 #!/usr/bin/env python3
 
 import os
-
 import grass.script as gs
 
+def run_hand_model(env):
+    gs.run_command("r.hydrodem", input="elev_lid792_1m", output="hydro_dem", env=env)
+    gs.run_command("r.stream.extract", elevation="hydro_dem", threshold=5000, output="streams", env=env)
+    gs.run_command("r.stream.distance", stream_rast="streams", direction="flow_dir", elevation="hydro_dem", output="HAND", env=env)
 
-def run_slope(scanned_elev, env, **kwargs):
-    gs.run_command(
-        "v.buffer",
-        input="firestations",
-        output="buffers",
-        type="point",
-        distance=20,
-        env=env,
-    )
+def run_surface_water(env):
+    gs.run_command("r.sim.water", elevation="elev_lid792_1m", rain_value=20, depth="flow_depth", output="water_flow", env=env)
 
+def run_flood_inundation(env):
+    gs.run_command("r.lake", elevation="elev_lid792_1m", water_level=5, output="flood_area", env=env)
 
 def main():
     env = os.environ.copy()
     env["GRASS_OVERWRITE"] = "1"
-    elevation = "elev_lid792_1m"
-    elev_resampled = "elev_resampled"
-    gs.run_command("g.region", raster=elevation, res=4, flags="a", env=env)
-    gs.run_command("r.resamp.stats", input=elevation, output=elev_resampled, env=env)
 
-    run_slope(scanned_elev=elev_resampled, env=env)
-
+    run_hand_model(env)
+    run_surface_water(env)
+    run_flood_inundation(env)
 
 if __name__ == "__main__":
     main()
-
