2a912fae2b1e29a7f8d1618afc37619792eb9e6d
mribeir3 <mribeir3@ncsu.edu>
2025-04-08 18:30:08 -0400

    Add files via upload

--- MatheusRLake.py
+++ MatheusRLake.py
@@ -4,13 +4,11 @@ import os
 import grass.script as gs
 
 
-def run_flood_simulation(scanned_elev, env, **kwargs):
-
-    # Simulate lake flooding from given coordinates
+def run_flood_simulation(elevation, env, **kwargs):
     gs.run_command(
         "r.lake",
-        elevation=scanned_elev,
-        water_level=120.5,
+        elevation=elevation,
+        water_level=121.0,
         lake="flood1",
         coordinates=(638728, 220278),
         env=env,
@@ -18,13 +16,14 @@ def run_flood_simulation(scanned_elev, env, **kwargs):
     print("Lake flooding simulation completed!")
 
 
-
 def main():
     env = os.environ.copy()
     env["GRASS_OVERWRITE"] = "1"
+    elevation = "elev_lid792_1m"
 
-    run_flood_simulation(env)
-    display_flood_result()
+    gs.run_command("g.region", raster=elevation, res=2, flags="a", env=env)
+
+    run_flood_simulation(elevation=elevation, env=env)
 
 
 if __name__ == "__main__":
